70f62d06e796947f05d7f91be1be993afe44e002
khidir000 <33225654+khidir000@users.noreply.github.com>
2021-01-08 11:31:08 +0800

    Create knn.py
    first commit

---
+++ knn.py
@@ -0,0 +1,17 @@
+from sklearn.neighbors import KNeighborsClassifier
+import time
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.model_selection import cross_val_score
+x = pd.read_csv('datafix2.csv')
+y = pd.read_csv('label.csv')
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1, stratify=y)
+n=20
+knn = KNeighborsClassifier(n_neighbors=n)
+knn.fit(x_train,y_train)
+knnsplit = knn.score(x_test,y_test)
+cv_scores = cross_val_score(knn, x, y, cv=2)
+
+print(cv_scores)
+print(knnsplit)
